7fc25d2c74f36b99193aafda7fd5a066b5337fe9
Lindsay Cerqueira <lindsaycerqueira@MacBook-Pro.local>
2020-10-01 15:18:27 -0300

    Enum HttpStatusCode exportável no index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
+import { HttpStatusCode } from "./enums/HttpStatusCode";
 import { Flawproof } from "./Flawproof";
 import { IFlawproof } from "./interfaces/IFlawproof";
 import { ErrorType } from "./types/ErrorType";
 
-export { Flawproof, IFlawproof, ErrorType };
+export { Flawproof, IFlawproof, ErrorType, HttpStatusCode };
